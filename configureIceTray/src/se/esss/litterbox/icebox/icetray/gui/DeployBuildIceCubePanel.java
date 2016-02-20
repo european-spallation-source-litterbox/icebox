bef9c9f786e19d5f681df029477314601618d441
stevemolloy <stephen.molloy@ice-tech.systems>
2016-02-21 00:39:21 +0100

    Convert the deployment panel into a class

---
+++ configureIceTray/src/se/esss/litterbox/icebox/icetray/gui/DeployBuildIceCubePanel.java
@@ -0,0 +1,13 @@
+package se.esss.litterbox.icebox.icetray.gui;
+
+import javax.swing.JPanel;
+
+public class DeployBuildIceCubePanel extends JPanel {
+
+	private static final long serialVersionUID = -6782526862914742436L;
+
+	public DeployBuildIceCubePanel() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
