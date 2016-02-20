99418ebae2b3d9191e7d4a69fab66d61b23e2aa6
stevemolloy <stephen.molloy@ice-tech.systems>
2016-02-20 11:32:07 +0100

    Rebuild after some merge conflicts.

---
+++ configureIceTray/src/se/esss/litterbox/icebox/icetray/icecube/Signal.java
@@ -0,0 +1,50 @@
+package se.esss.litterbox.icebox.icetray.icecube;
+
+import javax.json.JsonObject;
+
+public abstract class Signal {
+
+	private final String name;
+
+	public Signal(JsonObject jsonInput) {
+		this.name = jsonInput.getString("name");
+	}
+	
+	public Signal(String nameInput) {
+		this.name = nameInput;
+	}
+	
+	protected String epicsDBFieldString(String key, String value) {
+		String outString = "field(";
+		outString += key;
+		outString += ", ";
+		outString += "\"" + value + "\"";
+		outString += ")\n";
+		return outString;
+	}
+	
+	public abstract String writeEPICSRecord(String fileName);
+	
+	public abstract String writeEPICSProtoFunc(char sig);
+	
+	protected abstract void buildJsonRep();
+	
+	public abstract JsonObject getJsonRep();
+
+	public abstract boolean isRead();
+	
+	public boolean isWrite() {
+		return !isRead();
+	}
+	
+	public String toString() {
+		return getName();
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public abstract boolean equals(Object inputObject);
+	
+}
---
+++ configureIceTray/src/se/esss/litterbox/icebox/icetray/icecube/WriteSignal.java
@@ -0,0 +1,81 @@
+package se.esss.litterbox.icebox.icetray.icecube;
+
+import javax.json.Json;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObject;
+
+public class WriteSignal extends Signal {
+
+	private final String pvName;
+	private final String recordType = "ao";
+	private JsonObject jsonRep;
+
+	public WriteSignal(JsonObject jsonInput) {
+		super(jsonInput);
+		this.pvName = getName() + ":set";
+		
+		buildJsonRep();
+	}
+	
+	public WriteSignal(String nameInput) {
+		super(nameInput);
+		this.pvName = getName() + ":set";
+		
+		buildJsonRep();
+	}
+	
+	public JsonObject getJsonRep() {
+		return jsonRep;
+	}
+
+	@Override
+	public String writeEPICSRecord(String fileName) {
+		String outString = "record(" + getRecordType() + ", " + getPvName() + ") {\n";
+		outString += "\t" + epicsDBFieldString("DTYP", "stream");
+		outString += "\t" + epicsDBFieldString("OUT", "@"+fileName+" set_"+getName()+"() $(PORT)");
+		outString += "}\n";
+		return outString;
+	}
+
+	@Override
+	public String writeEPICSProtoFunc(char sig) {
+		String outString = "set_" + getName() + " {\n";
+		outString += "\tout \"" + sig + "%d\\n\";\n";
+		outString += "\tExtraInput = Ignore;\n";
+		outString += "}\n";
+		return outString;
+	}
+	
+	@Override
+	protected void buildJsonRep() {
+		JsonBuilderFactory factory = Json.createBuilderFactory(null);
+		jsonRep = factory.createObjectBuilder()
+			     .add("name", getName())
+			     .add("RW", "W")
+			     .build();
+	}
+	
+	@Override
+	public boolean isRead() {
+		return false;
+	}
+
+	public String getPvName() {
+		return pvName;
+	}
+
+	public String getRecordType() {
+		return recordType;
+	}
+
+	public boolean equals(Object inputObject) {
+		if (this==inputObject) return true;
+		if (this.getClass() != inputObject.getClass()) return false;
+		
+		WriteSignal inputSignal = (WriteSignal) inputObject;
+		
+		return this.pvName.equals(inputSignal.pvName)
+				&& this.recordType.equals(inputSignal.recordType)
+				&& this.getName().equals(inputSignal.getName());
+	}
+}
