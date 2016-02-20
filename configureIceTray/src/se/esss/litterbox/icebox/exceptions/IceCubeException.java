33de5763291184bf7e2172d77e975686b63c284c
stevemolloy <stephen.molloy@ice-tech.systems>
2016-02-20 21:37:49 +0100

    Added a string constructor to the Exception class

--- configureIceTray/src/se/esss/litterbox/icebox/exceptions/IceCubeException.java
+++ configureIceTray/src/se/esss/litterbox/icebox/exceptions/IceCubeException.java
@@ -3,5 +3,13 @@ package se.esss.litterbox.icebox.exceptions;
 public class IceCubeException extends Exception {
 
 	private static final long serialVersionUID = 3084525617405997006L;
+	
+	public IceCubeException() {
+		
+	}
+	
+	public IceCubeException(String message) {
+		super(message);
+	}
 
 }
