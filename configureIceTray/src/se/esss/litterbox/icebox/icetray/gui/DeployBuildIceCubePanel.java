15dcd7c41cf062dd878b38a9c4f9ce534bc2b7e8
stevemolloy <stephen.molloy@ice-tech.systems>
2016-02-21 17:39:11 +0100

    Rudimentary ActionListeners added to the build'n'deploy panel buttons

--- configureIceTray/src/se/esss/litterbox/icebox/icetray/gui/DeployBuildIceCubePanel.java
+++ configureIceTray/src/se/esss/litterbox/icebox/icetray/gui/DeployBuildIceCubePanel.java
@@ -1,8 +1,11 @@
 package se.esss.litterbox.icebox.icetray.gui;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class DeployBuildIceCubePanel extends JPanel {
@@ -10,14 +13,39 @@ public class DeployBuildIceCubePanel extends JPanel {
 	private static final long serialVersionUID = -6782526862914742436L;
 	private JButton btnBuildIcecube;
 	private JButton btnBuildDeploy;
+	
+	private ActionListener buildBtnListener = new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JOptionPane.showMessageDialog(
+					btnBuildIcecube, 
+					"Build Btn pressed!"
+					);
+		}
+	};
+	private ActionListener buildDeployBtnListener = new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JOptionPane.showMessageDialog(
+					btnBuildIcecube, 
+					"Build&Deploy Btn pressed!"
+					);
+		}
+	};
 
 	public DeployBuildIceCubePanel() {
 		setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		btnBuildIcecube = new JButton("Build IceCube");
 		btnBuildDeploy = new JButton("Build & Deploy IceCube");
+		
+		btnBuildIcecube.addActionListener(buildBtnListener);
+		btnBuildDeploy.addActionListener(buildDeployBtnListener);
+		
 		add(btnBuildIcecube);
-		add(btnBuildDeploy);		
+		add(btnBuildDeploy);
 	}
-
+	
 }
