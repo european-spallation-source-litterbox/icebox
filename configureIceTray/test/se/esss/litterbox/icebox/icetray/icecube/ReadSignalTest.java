3edb789a5a677820328d641b16fdf49c966d1ef0
Stephen Molloy <sdmolloy@gmail.com>
2016-02-28 22:23:23 +0100

    First creation of a UnitTest class

---
+++ configureIceTray/test/se/esss/litterbox/icebox/icetray/icecube/ReadSignalTest.java
@@ -0,0 +1,109 @@
+package se.esss.litterbox.icebox.icetray.icecube;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReadSignalTest {
+
+	@Test
+	public void testWriteEPICSRecord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWriteEPICSProtoFunc() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuildJsonRep() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetJsonRep() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPvExt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsRead() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadSignalJsonObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadSignalStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadSignalString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetScanRate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRecordType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSignalJsonObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSignalString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEpicsDBFieldString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsWrite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPvName() {
+		fail("Not yet implemented");
+	}
+
+}
