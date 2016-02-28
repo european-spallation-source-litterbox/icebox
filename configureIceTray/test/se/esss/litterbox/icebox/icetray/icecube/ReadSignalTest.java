7611b65009756a677fec15f192cac069dac81c17
Stephen Molloy <sdmolloy@gmail.com>
2016-02-29 00:03:04 +0100

    Extra tests

--- configureIceTray/test/se/esss/litterbox/icebox/icetray/icecube/ReadSignalTest.java
+++ configureIceTray/test/se/esss/litterbox/icebox/icetray/icecube/ReadSignalTest.java
@@ -3,6 +3,7 @@ package se.esss.litterbox.icebox.icetray.icecube;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import se.esss.litterbox.icebox.exceptions.SignalException;
@@ -10,35 +11,42 @@ import se.esss.litterbox.icebox.exceptions.SignalException;
 public class ReadSignalTest {
 	
 	ReadSignal testReadSignal;
+	String sigName = "testName";
 	
 	@Before
 	public void setUp() throws SignalException {
-		testReadSignal = new ReadSignal("testName");
+		testReadSignal = new ReadSignal(sigName);
 	}
 
 	@Test
+	@Ignore
 	public void testWriteEPICSRecord() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testWriteEPICSProtoFunc() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testBuildJsonRep() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testGetJsonRep() {
 		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetPvExt() {
-		fail("Not yet implemented");
+		assertEquals("PV extension should be \":get\"", 
+				":get", 
+				testReadSignal.getPvExt());
 	}
 
 	@Test
@@ -47,71 +55,85 @@ public class ReadSignalTest {
 	}
 
 	@Test
+	@Ignore
 	public void testEqualsObject() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testReadSignalJsonObject() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testReadSignalStringString() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testReadSignalString() {
 		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetScanRate() {
-		fail("Not yet implemented");
+		assertEquals("scanRate should be \".1 second\"", 
+				".1 second", 
+				testReadSignal.getScanRate());
 	}
 
 	@Test
 	public void testGetRecordType() {
-		fail("Not yet implemented");
+		assertEquals("ReadSignals should have recordtype \"ai\"", 
+				"ai", 
+				testReadSignal.getRecordType());
 	}
 
 	@Test
+	@Ignore
 	public void testSignalJsonObject() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testSignalString() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testEpicsDBFieldString() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testCheckName() {
 		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testIsWrite() {
-		fail("Not yet implemented");
+		assertFalse("iswrite() should be false for ReadSignals", testReadSignal.isWrite());
 	}
 
 	@Test
+	@Ignore
 	public void testToString() {
 		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetName() {
-		fail("Not yet implemented");
+		assertEquals(sigName, testReadSignal.getName());
 	}
 
 	@Test
+	@Ignore
 	public void testGetPvName() {
 		fail("Not yet implemented");
 	}
