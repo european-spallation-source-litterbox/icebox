bb8fc5408bd0f639a8a756aa6579541a834f0854
stevemolloy <stephen.molloy@ice-tech.systems>
2016-03-05 23:12:52 +0100

    A new test class. Not yet implemented.

---
+++ configureIceTray/src/test/java/se/esss/litterbox/icebox/icetray/IceCubeTest.java
@@ -0,0 +1,34 @@
+package se.esss.litterbox.icebox.icetray;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IceCubeTest {
+
+	@Test
+	public void testIceCubeJsonObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testIceCubeStringListOfSignal() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetEpicsDBString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetEpicsProtoString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCountSignals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
